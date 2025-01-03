eff59c93763134424ebcc3706992a7d75977b7c3
chamodmaduvantha <90128831+chamodmaduvantha@users.noreply.github.com>
2025-01-03 15:31:39 +0530

    About Us
    Container

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -11,7 +11,8 @@
         <div class="center-container">
             <v-row>
                 <v-col cols="12" sm="6">
-                    <v-img data-aos="zoom-out" data-aos-duration="2000" src="@/assets/AboutUs.png" align="center"></v-img>
+                    <v-img data-aos="zoom-out" data-aos-duration="2000" src="@/assets/AboutUs.png"
+                        align="center"></v-img>
                 </v-col>
                 <v-col cols="12" sm="6">
                     <p class="paragraph" data-aos="fade-up" data-aos-duration="3000">
@@ -37,25 +38,39 @@
 
         <v-divider></v-divider>
 
-        <h1 class="title mt-5" data-aos="fade-up" data-aos-duration="3000">
-            <span class="highlight">OUR VISION</span> & <span class="highlight">OUR MISSION</span>
-        </h1>
+        <div class="center-container">
+            <h1 class="title mt-5" data-aos="fade-up" data-aos-duration="3000">
+                <span class="highlight">OUR VISION</span> & <span class="highlight">OUR MISSION</span>
+            </h1>
+        </div>
+        <div class="center-container">
+            <v-row class="mt-4">
+                <v-col cols="12" sm="6">
+                    <v-img data-aos="zoom-out" data-aos-duration="2000" src="@/assets/MissionVision.png"
+                        align="center"></v-img>
+                </v-col>
 
-        <v-row class="mt-4">
-            <v-col cols="12" sm="6">
-                <v-img data-aos="zoom-out" data-aos-duration="2000" src="@/assets/MissionVision.png" align="center"></v-img>
-            </v-col>
-
-            <v-col cols="12" sm="6">
-                <p class="paragraph mt-3" data-aos="fade-up" data-aos-duration="3000"><b>Our Mission </b>is to excel as a premier Cambridge English Assessment preparation center in Sri Lanka. We are dedicated to providing an exceptional learning experience, fostering language proficiency, critical thinking, and global awareness. Through innovative teaching and a commitment to student success, we aim to be the preferred choice for effective Cambridge English Assessment preparation, making a lasting impact on our students' academic and personal growth.</p>
-            </v-col>
-        </v-row>
-        <v-row class="mt-4">
-            <v-col cols="12">
-                <p class="paragraph mt-3" data-aos="fade-up" data-aos-duration="3000"><b>Our Vision </b>is to lead as the top Cambridge English Assessment preparation center in Sri Lanka, dedicated to fostering excellence in language proficiency and academic success. We aim to provide an inspiring learning environment, utilizing innovative teaching methods and resources to empower students for success in a globalized world.</p>
-            </v-col>
-        </v-row>
-
+                <v-col cols="12" sm="6">
+                    <p class="paragraph mt-3" data-aos="fade-up" data-aos-duration="3000"><b>Our Mission </b>is to excel
+                        as a premier Cambridge English Assessment preparation center in Sri Lanka. We are dedicated to
+                        providing an exceptional learning experience, fostering language proficiency, critical thinking,
+                        and global awareness. Through innovative teaching and a commitment to student success, we aim to
+                        be the preferred choice for effective Cambridge English Assessment preparation, making a lasting
+                        impact on our students' academic and personal growth.</p>
+                </v-col>
+            </v-row>
+        </div>
+        <div class="center-container">
+            <v-row class="mt-4">
+                <v-col cols="12">
+                    <p class="paragraph mt-3" data-aos="fade-up" data-aos-duration="3000"><b>Our Vision </b>is to lead
+                        as the top Cambridge English Assessment preparation center in Sri Lanka, dedicated to fostering
+                        excellence in language proficiency and academic success. We aim to provide an inspiring learning
+                        environment, utilizing innovative teaching methods and resources to empower students for success
+                        in a globalized world.</p>
+                </v-col>
+            </v-row>
+        </div>
 
 
         <!-- Existing content -->
