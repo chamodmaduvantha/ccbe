f58e70281f39d27477b69f63b25b05483c92eaa4
chamodmaduvantha <90128831+chamodmaduvantha@users.noreply.github.com>
2025-01-03 11:25:37 +0530

    Footer
    Update Footer Styling

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,17 +1,20 @@
 <template>
-    <v-footer class="text-center d-flex flex-column mt-5" color="#E65100">
+    <v-footer class="text-center d-flex flex-column mt-5" color="#5E0000">
         <div class="footer">
             <div class="mt-2">
-                <strong>Shaping the Future of Education</strong>
+                <strong class="title-footer">Shaping the Future of Education</strong>
             </div>
             <div>
                 <v-btn v-for="icon in icons" :key="icon.name" class="mx-4" variant="text" @click="navigate(icon.link)">
                     <v-icon>{{ icon.name }}</v-icon> <!-- Display the icon -->
                 </v-btn>
             </div>
-            <img width="100" height="100" class="ma-auto" :src="logo" @click="navigate(icons[0].link)" />
+            <img width="150" height="150" class="ma-auto mr-3" :src="logo" @click="navigate(icons[0].link)" />
+            <v-divider vertical></v-divider>
+            <img width="150" height="100" class="ma-auto ml-3" :src="lable" @click="navigate(icons[0].link)" />
             <div class="pt-0">
-                At Cambridge Collage of British English, we are committed to empowering students with the tools they need to succeed academically and beyond.
+                At Cambridge Collage of British English, we are committed to empowering students with the tools they
+                need to succeed academically and beyond.
                 <br>Copyright © 2025 Cambridge Collage of British English. All rights reserved.
             </div>
         </div>
@@ -23,6 +26,7 @@ export default {
     data() {
         return {
             logo: require('@/assets/Logo.png'),
+            lable: require('@/assets/lable.jpeg'),
             icons: [
                 { name: 'mdi-instagram', link: 'https://www.instagram.com/ccbeamb' },
                 { name: 'mdi-facebook', link: 'https://www.facebook.com/ccbe.ambalangoda' },
@@ -41,6 +45,9 @@ export default {
 
 <style>
 .footer {
-  justify-content: center;
+    justify-content: center;
+}
+.title-footer{
+    font-size: 25px;
 }
 </style>
