b2ed831c74151e70d69dc77dbad81c3357a466d7
chamodmaduvantha <90128831+chamodmaduvantha@users.noreply.github.com>
2025-01-05 10:57:30 +0530

    Course
    Update the Our Causes Section

--- src/components/CoursePage.vue
+++ src/components/CoursePage.vue
@@ -5,151 +5,98 @@
             OUR <span class="highlight">COURSES</span>
         </h1>
         <br>
-
-        <v-timeline align="start" side="end">
-            <v-timeline-item dot-color="pink" size="small">
-                <v-card color="teal-darken-4" max-width="450">
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                        <div>
-                            <v-card-title class="text-h5">Pre School</v-card-title>
-                            <v-card-subtitle>Little Steps, Big Dreams</v-card-subtitle>
-                            <v-card-actions>
-                                <v-btn class="custom-btn mt-3" variant="outlined" color="yellow">
-                                    View Details
-                                </v-btn>
-                            </v-card-actions>
-                        </div>
-                        <v-avatar class="ma-3" rounded="0" size="125">
-                            <v-img
-                                src="https://www.parents.com/thmb/d0qdbY82J3o-2ezdehypjSNXKI0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Parents-Preschool-Age-Stocksy-3668758-4b6915ed8fbd4d32aa00d86fc7ad43ad.jpg"></v-img>
-                        </v-avatar>
-                    </div>
-                </v-card>
-            </v-timeline-item>
-            <v-timeline-item dot-color="pink" size="small">
-                <v-card color="teal-darken-4" max-width="450">
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                        <div>
-                            <v-card-title class="text-h5">Young Learners</v-card-title>
-                            <v-card-subtitle>Age 6 - 12</v-card-subtitle>
-                            <v-card-actions>
-                                <v-btn class="custom-btn mt-3" variant="outlined" color="yellow">
-                                    View Details
-                                </v-btn>
-                            </v-card-actions>
-                        </div>
-                        <v-avatar class="ma-3" rounded="0" size="125">
-                            <v-img
-                                src="https://images.kent.gov.uk/0020/171362/Primary-school-children.jpeg?tr=n-news_og"></v-img>
-                        </v-avatar>
-                    </div>
-                </v-card>
-            </v-timeline-item>
-            <v-timeline-item dot-color="pink" size="small">
-                <v-card color="teal-darken-4" max-width="450">
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                        <div>
-                            <v-card-title class="text-h5">For School</v-card-title>
-                            <v-card-subtitle>Age 13 - 18</v-card-subtitle>
-                            <v-card-actions>
-                                <v-btn class="custom-btn mt-3" variant="outlined" color="yellow">
-                                    View Details
-                                </v-btn>
-                            </v-card-actions>
-                        </div>
-                        <v-avatar class="ma-3" rounded="0" size="125">
-                            <v-img
-                                src="https://edsource.org/wp-content/uploads/2021/12/Oak_Tech_137-1024x683.jpg"></v-img>
-                        </v-avatar>
-                    </div>
-                </v-card>
-            </v-timeline-item>
-            <v-timeline-item dot-color="pink" size="small">
-                <v-card color="teal-darken-4" max-width="450">
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                        <div>
-                            <v-card-title class="text-h5">English Skills for O/L's</v-card-title>
-                            <v-card-subtitle>Age 6 - 12</v-card-subtitle>
-                            <v-card-actions>
-                                <v-btn class="custom-btn mt-3" variant="outlined" color="yellow">
-                                    View Details
-                                </v-btn>
-                            </v-card-actions>
-                        </div>
-                        <v-avatar class="ma-3" rounded="0" size="125">
-                            <v-img src="https://britishway.lk/wp-content/uploads/2019/11/course_img.jpg"></v-img>
-                        </v-avatar>
-                    </div>
-                </v-card>
-            </v-timeline-item>
-            <v-timeline-item dot-color="pink" size="small">
-                <v-card color="teal-darken-4" max-width="450">
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                        <div>
-                            <v-card-title class="text-h5">English Skills for A/L's</v-card-title>
-                            <v-card-subtitle>Age 13 - 18</v-card-subtitle>
-                            <v-card-actions>
-                                <v-btn class="custom-btn mt-3" variant="outlined" color="yellow">
-                                    View Details
-                                </v-btn>
-                            </v-card-actions>
-                        </div>
-                        <v-avatar class="ma-3" rounded="0" size="125">
-                            <v-img
-                                src="https://www.ethoseducation.lk/wp-content/uploads/2022/08/shutterstock_611097329.jpg"></v-img>
-                        </v-avatar>
-                    </div>
-                </v-card>
-            </v-timeline-item>
-            <v-timeline-item dot-color="pink" size="small">
-                <v-card color="teal-darken-4" max-width="450">
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                        <div>
-                            <v-card-title class="text-h5">IELTS</v-card-title>
-                            <v-card-subtitle>Age 6 - 12</v-card-subtitle>
-                            <v-card-actions>
-                                <v-btn class="custom-btn mt-3" variant="outlined" color="yellow">
-                                    View Details
-                                </v-btn>
-                            </v-card-actions>
-                        </div>
-                        <v-avatar class="ma-3" rounded="0" size="125">
-                            <v-img
-                                src="https://specialistlanguagecourses.com/wp-content/uploads/2023/12/Depositphotos_68789187_XL-scaled-1-600x401.jpg"></v-img>
-                        </v-avatar>
-                    </div>
-                </v-card>
-            </v-timeline-item>
-        </v-timeline>
         <v-row>
-
             <v-col cols="12" sm="1" md="6">
-                
+                <v-card class="mx-auto" max-width="400" data-aos="fade-up" data-aos-duration="3000" elevation="23">
+                    <v-img class="align-end text-white image-pop-up" height="200" src="https://www.parents.com/thmb/d0qdbY82J3o-2ezdehypjSNXKI0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Parents-Preschool-Age-Stocksy-3668758-4b6915ed8fbd4d32aa00d86fc7ad43ad.jpg" cover></v-img>
+                    <v-card-subtitle class="pt-4"></v-card-subtitle>
+                    <v-card-text class="cardtext">
+                        <h3><b>Pre Schoolers</b></h3>
+                        <div>This program is for children who know their ABCs and are just starting to read or have some familiarity with English.</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="custom-btn mt-3" variant="outlined" color="teal-darken-4">Learn More -></v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-col>
 
             <v-col cols="12" sm="1" md="6">
-                
-            </v-col>
-
-            <v-col cols="12" sm="1" md="6">
-                
-            </v-col>
-            <v-col cols="12" sm="1" md="6">
-                
-            </v-col>
-
-            <v-col cols="12" sm="1" md="6">
-                
-            </v-col>
-            <v-col cols="12" sm="1" md="6">
-                
+                <v-card class="mx-auto" max-width="400" data-aos="fade-up" data-aos-duration="3000" elevation="23">
+                    <v-img class="align-end text-white image-pop-up" height="200" src="https://images.kent.gov.uk/0020/171362/Primary-school-children.jpeg?tr=n-news_og" cover></v-img>
+                    <v-card-subtitle class="pt-4"></v-card-subtitle>
+                    <v-card-text class="cardtext">
+                        <h3><b>Young Learners (Age 6-12)</b></h3>
+                        <div>Want to customize Elementor image box height? Here we have the Solution!</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="custom-btn mt-3" variant="outlined" color="teal-darken-4">Learn More -></v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-col>
         </v-row>
 
+        <v-row>
+            <v-col cols="12" sm="1" md="6">
+                <v-card class="mx-auto" max-width="400" data-aos="fade-up" data-aos-duration="3000" elevation="23">
+                    <v-img class="align-end text-white image-pop-up" height="200" src="https://edsource.org/wp-content/uploads/2021/12/Oak_Tech_137-1024x683.jpg" cover></v-img>
+                    <v-card-subtitle class="pt-4"></v-card-subtitle>
+                    <v-card-text class="cardtext">
+                        <h3><b>For Schools (Age 13-18)</b></h3>
+                        <div>Want to customize Elementor image box height? Here we have the Solution!</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="custom-btn mt-3" variant="outlined" color="teal-darken-4">Learn More -></v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="1" md="6">
+                <v-card class="mx-auto" max-width="400" data-aos="fade-up" data-aos-duration="3000" elevation="23">
+                    <v-img class="align-end text-white image-pop-up" height="200" src="https://britishway.lk/wp-content/uploads/2019/11/course_img.jpg" cover></v-img>
+                    <v-card-subtitle class="pt-4"></v-card-subtitle>
+                    <v-card-text class="cardtext">
+                        <h3><b>English Skills For O/L's</b></h3>
+                        <div>Trying to add image box to your WordPress sites? No worries!</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="custom-btn mt-3" variant="outlined" color="teal-darken-4">Learn More -></v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+
+        <v-row>
+            <v-col cols="12" sm="1" md="6">
+                <v-card class="mx-auto" max-width="400" data-aos="fade-up" data-aos-duration="3000" elevation="23">
+                    <v-img class="align-end text-white image-pop-up" height="200" src="https://www.ethoseducation.lk/wp-content/uploads/2022/08/shutterstock_611097329.jpg" cover></v-img>
+                    <v-card-subtitle class="pt-4"></v-card-subtitle>
+                    <v-card-text class="cardtext">
+                        <h3><b>English Skills For A/L's</b></h3>
+                        <div>Want to customize Elementor image box height? Here we have the Solution!</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="custom-btn mt-3" variant="outlined" color="teal-darken-4">Learn More -></v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+
+            <v-col cols="12" sm="1" md="6">
+                <v-card class="mx-auto" max-width="400" data-aos="fade-up" data-aos-duration="3000" elevation="23">
+                    <v-img class="align-end text-white image-pop-up" height="200" src="https://specialistlanguagecourses.com/wp-content/uploads/2023/12/Depositphotos_68789187_XL-scaled-1-600x401.jpg" cover></v-img>
+                    <v-card-subtitle class="pt-4"></v-card-subtitle>
+                    <v-card-text class="cardtext">
+                        <h3><b>IELTS</b></h3>
+                        <div>Do you know how to add Elementor image box with button?</div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn class="custom-btn mt-3" variant="outlined" color="teal-darken-4">Learn More -></v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
     <FooterPage></FooterPage>
 </template>
-
 
 <script>
 import NavBar from './NavBar.vue';
@@ -176,7 +123,6 @@ export default {
 };
 </script>
 
-
 <style>
 .title {
     text-align: center;
@@ -188,74 +134,28 @@ export default {
     color: #5E0000;
 }
 
-.subtitle {
-    font-size: 1.2rem;
-    color: #555;
-    margin: 20px 0;
-}
-
-.title-intro {
-    font-family: "Anton", sans-serif;
-    font-weight: 400;
-    font-style: normal;
-    text-align: center;
-}
-
-.paragraph {
-    font-family: "Roboto", sans-serif;
-    font-weight: 14px;
-    font-size: 20px;
-    font-style: normal;
+.cardtext {
+    color: #5E0000;
+    font-size: 15px;
     text-align: justify;
 }
 
-#logo {
-    justify-content: center;
+.image-pop-up {
+    transition: transform 0.3s ease-out, box-shadow 0.3s ease-out;
 }
 
-h4 {
-    justify-content: center;
-    text-align: center;
+.v-card:hover .image-pop-up {
+    transform: scale(1.1); /* Pop-up effect */
+    box-shadow: 0 12px 24px rgba(0, 0, 0, 0.3); /* Shadow for more depth */
 }
 
-.logo-container {
-    display: flex;
-    justify-content: center;
-    /* Center horizontally */
-    align-items: center;
-    margin-top: 20px;
+.v-card {
+    position: relative;
+    transition: transform 0.3s ease-out, box-shadow 0.3s ease-out;
 }
 
-ul {
-    list-style: none;
-    /* Optional: remove bullet points */
-    padding: 0;
-    margin: 0 auto;
-    /* Centers the list horizontally */
-    text-align: center;
-    /* Centers the text within the list items */
-}
-
-li {
-    display: inline-block;
-    /* Optional: makes list items align horizontally */
-    margin: 0 10px;
-    /* Adjust spacing between items */
-}
-
-.center-container {
-    display: flex;
-    justify-content: center;
-    align-items: left;
-    display: flex;
-}
-
-.v-card--reveal {
-    align-items: center;
-    bottom: 0;
-    justify-content: center;
-    opacity: .9;
-    position: absolute;
-    width: 100%;
+.v-card:hover {
+    transform: translateY(-5px); /* Slight lift for the card */
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2); /* Card shadow */
 }
 </style>
